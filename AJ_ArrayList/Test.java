160ef152ff7d074f4b9c1406148c0cfee484b42d
Dronnnnn <andreishalyopa@gmail.com>
2023-02-25 20:23:03 +0200

    New classes: ArrayDequeExample.class

--- AJ_ArrayList/Test.java
+++ AJ_ArrayList/Test.java
@@ -5,15 +5,16 @@ import java.util.List;
 
 public class Test {
 
-//    static int somMeth(){
-//        int temp = 10;
-//        return temp;
-//    }
-//    static void somMeth(String a){
-//        System.out.println("qweqeq"+ a);
-//    }
-//
-//    public static void main(String[] args) {
+    static int somMeth() {
+        int temp = 10;
+        return temp;
+    }
+
+    static void somMeth(String a) {
+        System.out.println("qweqeq" + a);
+    }
+
+    public static void main(String[] args) {
 //        List<Integer> list = new ArrayList<>();
 //        for (int i = 0; i < 10; i++) {
 //            list.add(i);
@@ -22,19 +23,19 @@ public class Test {
         //System.out.println(list.get(99));
         //System.out.println(list.size());
 
-       // for(int i = 0; i < list.size(); i++){
-       //     System.out.println(list.get(i));
-       // }
+        // for(int i = 0; i < list.size(); i++){
+        //     System.out.println(list.get(i));
+        // }
 //
-       // for(Integer x : list) {
-       //     System.out.println(x);
-       // }
+        // for(Integer x : list) {
+        //     System.out.println(x);
+        // }
 //        list.remove(5);
 //        System.out.println(list);
 //        System.out.println("for Git Update");
-//        somMeth("123");
-//        int b = somMeth();
-//        System.out.println(b);
-//    }
+        somMeth("123");
+        int b = somMeth();
+        System.out.println(b);
+    }
 }
 
